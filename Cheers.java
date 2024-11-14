38c8a954259d57454b11e0cf86827df18687436f
שירה מאור <shira.maor@post.runi.ac.il>
2024-11-15 00:42:58 +0200

    TimeCalc task

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,21 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String cheers = args [0];
+            int times = Integer.parseInt(args[1]);
+            String specialChar = "AEFHILMNORSX";
+            for (int i = 0; i < cheers.length(); i++) {
+                char firstChar = cheers.charAt(i);
+                if (firstChar == specialChar.indexOf(firstChar)) {
+                   System.out.println("Give me an " + cheers.charAt(i) + ": " + firstChar + "!" );    
+                } else {
+                   System.out.println("Give me a " + cheers.charAt(i) + ": " + firstChar + "!" );    
+                }     
+            }
+            System.out.println("What does that spell?");
+            for (int i = 0; i < times; i++) {
+                System.out.println(cheers + "!!!");
+            }
+               
         }
 }
