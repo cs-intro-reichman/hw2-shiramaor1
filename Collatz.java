ea72da5133f07209fe484af2d8dbb33dd0023705
שירה מאור <shira.maor@post.runi.ac.il>
2024-11-20 17:09:46 +0200

    style

--- Collatz.java
+++ Collatz.java
@@ -1,13 +1,12 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-		int N = Integer.parseInt(args[0]);
+	    int N = Integer.parseInt(args[0]);
 		String mode = args[1]; 
 		for ( int i = 1; i <= N; i++ ) {
 			int a = i; 
 			int times = 1;
-			String runs = "" + a + " "; 
-			
+			String runs = "" + a + ""; 
 			while (a != 1 || times == 1) {
 				if ( a % 2 == 0){
 					a = a / 2;
